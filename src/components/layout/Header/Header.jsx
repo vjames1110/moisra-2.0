7e3a879d6fdb95f7a2ae10eccc58e65e8808f0c9
vjames1110 <vikashjames8@gmail.com>
2026-07-02 13:26:16 +0530

    Logo changed again

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -5,7 +5,7 @@ import { IoClose } from "react-icons/io5";
 
 // Logo Import
 
-import logo from "../../../logos/moisra-logo-2.png"
+import logo from "../../../logos/moisra-logo-3.png"
 
 
 import MegaMenu from "../MegaMenu/MegaMenu";
